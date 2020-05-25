5366fd8086216fb356567023a5d21c6b59137b34
djordje_radivojevic_eng <djordje.radivojevic.eng@gmail.com>
2020-05-25 10:13:13 +0200

    Initial commit

---
+++ src/main/java/com/springframework/chucknorisjokes/ChucknorisjokesApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.chucknorisjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChucknorisjokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChucknorisjokesApplication.class, args);
+    }
+
+}
